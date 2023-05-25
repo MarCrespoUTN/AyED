55a3dced811d1f2604bf12e86d0dfc844c1d5349
Martin Crespo Utn <marcrespo@frba.utn.edu.ar>
2023-05-25 02:03:36 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -2,31 +2,27 @@
 #include <iomanip>
 using namespace std;
 
-  double pi(double n,double& x,double& g,double& d)                           //crear una funcion pi para realizar el calculo.
+
+
+  double pi() 				                     //Crear una funcion pi para realizar el calculo.
   {
-    n= 1;
-    g= 0;                                                                     // inserto variables.
-    x= 1;
-    
-    while(n<=1000000){                                                        //genero un while para repetir la sumatoria una determinada cantidad de veces.
-  	d = 2*n-1;                                                                // valor del denominador.
-  	g =  g + x*(1/d);                                                         //realizo el calculo con la serie de Liebniz.
-  	x = -x;                                                                   // Cambio el signo de la sumatoria.
-    n++;                                                                      //Aumento valor en n.
+      double n= 1;
+	    double g= 0;           							 //Inserto variables.
+	    double x= 1;
+      double d= 0;
+    while(n<=1000000){ 										 //Genero un while para repetir la sumatoria una determinada cantidad de veces.
+	  d= 2*n-1;											
+  	g =  g + x*(1/d);										   //Realizo el calculo con la serie de Liebniz.
+  	x *= -1; 														   //Cambio el signo de la sumatoria.
+    n++; 	                                 //Aumento valor en n.
                      }
-                     
-    return 1;
+    g= g*4;    
+    return g;
   }  
   int main()
   {
-  	double n;
-  	double x;                                                                 //inserto mismas variables que en mi funcion pi
-  	double g;
-  	double d;
-      
-  pi(n, x,  g, d);                                                            //llamo a mi funcion
-  cout<<setprecision(7)<<g*4;                                                 //Utilizo setprecision(7) para usar un valor fijo de comas y 
-                                                                              //Multiplico por 4 ya que la serie de liebniz me da pi/4.
-
-  return 0;                                                                   // finalizo el programa
+ 												                   //Llamo a mi funcion
+  cout<<setprecision(7)<<pi(); 						 //Utilizo setprecision(7) para usar un valor fijo de comas y 
+                                           //Multiplico por 4 ya que la serie de liebniz me da pi/4.
+  return 0;                                //Finalizo el programa
   }
